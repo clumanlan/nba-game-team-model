a0c354ccc1c7b9b94655529150dce4ca3e608891
carlylelumanlan <lumanlanc0@gmail.com>
2023-11-09 08:08:13 -0600

    add get data functions

---
+++ src/build-model-draft.py
@@ -0,0 +1,150 @@
+import pandas as pd
+import sklearn 
+import seaborn as sns
+from nba_api.stats.static import teams, players
+from nba_api.stats.endpoints import playergamelog, commonplayerinfo, ScoreboardV2, BoxScoreAdvancedV2, BoxScoreTraditionalV2
+from nba_api.stats.library.parameters import SeasonAll
+from nba_api.stats.static import players
+from datetime import date
+import awswrangler as wr
+import numpy as np
+
+# FUNCTIONS ---------------------------------------
+
+
+def get_game_headers() -> tuple:
+
+    game_stats_path = "s3://nbadk-model/game_stats"
+
+    game_headers_df = wr.s3.read_parquet(
+        path=game_stats_path,
+        path_suffix = ".parquet" ,
+        use_threads =True
+    )
+
+
+    game_headers_df_processed = (game_headers_df
+        .assign(
+            gametype_string = game_headers_df.GAME_ID.str[:3],
+            game_type = lambda x: np.where(x.gametype_string == '001', 'Pre-Season',
+                np.where(x.gametype_string == '002', 'Regular Season',
+                np.where(x.gametype_string == '003', 'All Star',
+                np.where(x.gametype_string == '004', 'Post Season',
+                np.where(x.gametype_string == '005', 'Play-In Tournament', 'unknown'))))),
+            GAME_ID = game_headers_df['GAME_ID'].astype(str),
+            GAME_DATE_EST = pd.to_datetime(game_headers_df['GAME_DATE_EST'])
+
+        )
+    )
+
+    game_headers_df_processed.drop_duplicates(subset=['GAME_ID', 'HOME_TEAM_ID', 'VISITOR_TEAM_ID'], inplace=True)
+
+    rel_cols = ['GAME_ID', 'game_type', 'SEASON', 'GAME_DATE_EST', 'HOME_TEAM_ID', 'VISITOR_TEAM_ID', 'HOME_TEAM_WINS', 'HOME_TEAM_LOSSES']
+
+    game_headers_df_processed_filtered = game_headers_df_processed[rel_cols]
+    game_headers_df_processed_filtered = game_headers_df_processed_filtered.drop_duplicates()
+
+    # Create long table in order to flag teams that are home or away
+    game_home_away = game_headers_df_processed[['GAME_ID','HOME_TEAM_ID', 'VISITOR_TEAM_ID']]
+    game_home_away = pd.melt(game_home_away, id_vars='GAME_ID', value_name='TEAM_ID', var_name='home_away')
+    game_home_away['home_away'] = game_home_away['home_away'].apply(lambda x: 'home' if x == 'HOME_TEAM_ID' else 'away')
+
+    return game_headers_df_processed_filtered, game_home_away
+
+
+def get_player_dfs() -> tuple:
+    """
+    Get dataframes for player information, traditional box score stats, and advanced box score stats 
+    for a given set of game IDs.
+
+    Args:
+        rel_game_ids (list): List of relevant game IDs to filter box score dataframes by.
+
+    Returns:
+        tuple: A tuple of three pandas dataframes: player_info_df, boxscore_trad_player_df, and boxscore_adv_player_df.
+    """
+
+    # Read in player information data
+    player_info_path = "s3://nbadk-model/player_info"
+    player_info_df = wr.s3.read_parquet(
+        path=player_info_path,
+        path_suffix=".parquet",
+        use_threads=True
+    )
+
+    # Clean player information data and rename columns
+    player_info_df = player_info_df[['PERSON_ID', 'HEIGHT', 'POSITION']].drop_duplicates()
+    player_info_df = player_info_df.rename({'PERSON_ID': 'PLAYER_ID'}, axis=1)
+
+    # Read in traditional box score data for players and filter by relevant game IDs
+    boxscore_trad_player_path = "s3://nbadk-model/player_stats/boxscore_traditional/"
+    boxscore_trad_player_df = wr.s3.read_parquet(
+        path=boxscore_trad_player_path,
+        path_suffix=".parquet",
+        use_threads=True
+    )
+    boxscore_trad_player_df['GAME_ID'] = boxscore_trad_player_df['GAME_ID'].astype(str)
+
+    # Read in advanced box score data for players and filter by relevant game IDs
+    boxscore_adv_player_path = "s3://nbadk-model/player_stats/boxscore_advanced/"
+    boxscore_adv_player_df = wr.s3.read_parquet(
+        path=boxscore_adv_player_path,
+        path_suffix=".parquet",
+        use_threads=True
+    )
+    boxscore_adv_player_df = boxscore_adv_player_df.drop_duplicates(subset=['GAME_ID','PLAYER_ID'])
+
+    return player_info_df, boxscore_trad_player_df, boxscore_adv_player_df
+
+
+
+def get_team_level_dfs() -> tuple:
+    """
+    Retrieve team level dataframes for the given game IDs.
+
+    Args:
+    rel_game_ids (list): A list of game IDs to filter the dataframes by.
+
+    Returns:
+    tuple: A tuple of two pandas dataframes, the first containing traditional team stats and the second containing advanced team stats.
+    """
+
+    # Read in traditional boxscore team stats
+    boxscore_trad_team_path = "s3://nbadk-model/team_stats/boxscore_traditional/"
+
+    boxscore_trad_team_df = wr.s3.read_parquet(
+        path=boxscore_trad_team_path,
+        path_suffix=".parquet",
+        use_threads=True
+    )
+
+    # Convert GAME_ID to string and filter by rel_game_ids
+    boxscore_trad_team_df['GAME_ID'] = boxscore_trad_team_df['GAME_ID'].astype(str)
+    boxscore_trad_team_df = boxscore_trad_team_df.drop_duplicates(subset=['GAME_ID', 'TEAM_ID'])
+
+    # Read in advanced boxscore team stats
+    boxscore_adv_team_path = "s3://nbadk-model/team_stats/boxscore_advanced/"
+
+    boxscore_adv_team_df = wr.s3.read_parquet(
+        path=boxscore_adv_team_path,
+        path_suffix=".parquet",
+        use_threads=True
+    )
+
+    # Drop duplicates and filter by rel_game_ids
+    boxscore_adv_team_df = boxscore_adv_team_df.drop_duplicates(subset=['GAME_ID', 'TEAM_ID'])
+
+    return boxscore_trad_team_df, boxscore_adv_team_df
+
+
+# GET ODDS DATA!!!!
+boxscore_trad_team_df, boxscore_adv_team_df = get_team_level_dfs()
+
+game_headers_df_processed_filtered, game_home_away = get_game_headers()
+
+
+
+boxscore_trad_team_df.columns
+
+boxscore_adv_team_df.columns
+
