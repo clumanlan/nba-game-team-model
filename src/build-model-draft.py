285613c7c424668457e56d23e19cd230d6f46161
carlylelumanlan <lumanlanc0@gmail.com>
2024-01-20 18:18:18 -0600

    add scoring metrics

--- src/build-model-draft.py
+++ src/build-model-draft.py
@@ -7,12 +7,25 @@ from nba_api.stats.library.parameters import SeasonAll
 from nba_api.stats.static import players
 from datetime import date
 import awswrangler as wr
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.base import TransformerMixin
+from sklearn.base import BaseEstimator, TransformerMixin
 import numpy as np
 import time
 import datetime as dt
 import mlflow
+import plotly.express as px
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from category_encoders import TargetEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+import xgboost as xgb
 
-# TODO: validate data :) create a baseline model, setup infrastrcutre for CI/CD to SAGEMAKER WITH MLFLOW
+
+# TODO: validate data :) create a baseline model, setup infrastrcutre for CI/CD to SAGEMAKER WITH MLFLOW, 
+##     could use pydantic to set column types of data coming in
 # USE MAPE? we'll use a bunch of different metric values 
 
 # check player dfs for missing game ids 
@@ -165,31 +178,181 @@ game_headers_filtered = (
     .drop(['HOME_TEAM_WINS', 'HOME_TEAM_LOSSES'], axis=1)
 )
 
-rel_game_ids = game_headers_filtered.GAME_ID.unique()
-
-
-game_headers_pivot = pd.melt(game_headers_filtered, id_vars=['GAME_ID','game_type', 'SEASON', 'GAME_DATE_EST'], value_vars=['HOME_TEAM_ID', 'VISITOR_TEAM_ID'], var_name='HOME_VISITOR', value_name='TEAM_ID')
-game_headers_pivot['HOME_VISITOR'] = game_headers_pivot['HOME_VISITOR'].replace({'HOME_TEAM_ID':'HOME', 'VISITOR_TEAM_ID':'VISITOR'})
-
+game_headers_filtered = pd.melt(game_headers_filtered, id_vars=['GAME_ID','game_type', 'SEASON', 'GAME_DATE_EST'], value_vars=['HOME_TEAM_ID', 'VISITOR_TEAM_ID'], var_name='HOME_VISITOR', value_name='TEAM_ID')
+game_headers_filtered['HOME_VISITOR'] = game_headers_filtered['HOME_VISITOR'].replace({'HOME_TEAM_ID':'HOME', 'VISITOR_TEAM_ID':'VISITOR'})
 
 
 boxscore_trad_team_df, boxscore_adv_team_df = get_team_level_dfs()
 
-bxscore_trad_irrelevant_cols = ['TEAM_ABBREVIATION', 'TEAM_CITY']
-boxscore_trad_team_df_filtered = boxscore_trad_team_df.drop(bxscore_trad_irrelevant_cols, axis=1)
 
-adv_team_team_identity_cols = ['TEAM_NAME', 'TEAM_ABBREVIATION', 'TEAM_CITY','MIN']
-boxscore_adv_team_df_filtered = boxscore_adv_team_df.drop(adv_team_team_identity_cols, axis=1)
+trad_cols_to_remove = ['TEAM_ABBREVIATION', 'TEAM_CITY']
+boxscore_trad_team_df = boxscore_trad_team_df.drop(trad_cols_to_remove, axis=1)
+
+adv_team_cols_to_remove = ['TEAM_NAME', 'TEAM_ABBREVIATION', 'TEAM_CITY','MIN']
+boxscore_adv_team_df = boxscore_adv_team_df.drop(adv_team_cols_to_remove, axis=1)
+
+team_boxscore_combined = pd.merge(boxscore_trad_team_df, boxscore_adv_team_df, on=['GAME_ID', 'TEAM_ID'], how='left')
+
+game_ids_missing = ['0020300778', '0022300023'] # check these later
+team_boxscore_combined = team_boxscore_combined[~team_boxscore_combined['GAME_ID'].isin(game_ids_missing)] # one game id missing trad & adv stats
+
+game_header_team_boxscore_combined = pd.merge(game_headers_filtered, team_boxscore_combined,  on=['GAME_ID', 'TEAM_ID'], how='inner')
+
+
+# CREATE A REGULAR SEASON BASELINE MODEL --------------------------------------------------------------------------
+
+
+game_team_regular = (
+    game_header_team_boxscore_combined[game_header_team_boxscore_combined['game_type']=='Regular Season']
+    .assign(
+        TEAM_ID = lambda x: x['TEAM_ID'].astype(str)
+    )
+    .sort_values(['GAME_DATE_EST', 'TEAM_ID'])
+    .reset_index(drop=True)
+)
+
+game_team_regular_train = game_team_regular[game_team_regular['SEASON']<2019].copy()
+
+for i in range(1,6):
+
+    ## accuracy flatlines at about 5 games out, with each game out happening there after only reducing by 0.1
+    col_label = f'team_lagged_pts_rolling_{i}_mean'
+    game_team_regular_train.loc[:,col_label] = game_team_regular_train.groupby(['TEAM_ID', 'SEASON'])['PTS'].transform(lambda x: x.shift(1).rolling(i, min_periods=i).mean())
+
+    print(col_label)
 
 
 
-# Setup Mlflow ------------------------------------------------
-## mlflow server --backend-store-uri sqlite:///mlflow.db 
-## --default-artifact-root mlruns/ 
+game_team_regular_five = game_team_regular_train[~game_team_regular_train['team_lagged_pts_rolling_5_mean'].isnull()]
+rolling_avg_five = game_team_regular_five['team_lagged_pts_rolling_5_mean']
+actual = game_team_regular_five['PTS']
+
+baseline_mse = mean_squared_error(actual, rolling_avg_five)
+baseline_rmse = np.sqrt(mean_squared_error(actual, rolling_avg_five))
+baseline_mae = mean_absolute_error(actual, rolling_avg_five)
+
+
+
+# create a simple model first with year, rolling averages, and home and away
+
+# numeric feature processing --------------------------------------------------
+rel_num_feats = ['SEASON'] + [col for col in game_team_regular_train.columns if 'pts_rolling' in col]
+
+num_pipeline = Pipeline(steps=[
+    ('scale', StandardScaler())
+])
+
+
+
+# cat feature processing ------------------------------------------------------------
+cat_cols_high_card = ['PLAYER_ID', 'TEAM_ID']
+
+cat_pipeline_high_card = Pipeline(steps=[
+    ('encoder', TargetEncoder(smoothing=2))
+])
+
+
+rel_cat_feats_low_card = ['HOME_VISITOR']
+
+cat_pipeline_low_card = Pipeline(steps=[
+    ('encoder', OneHotEncoder(handle_unknown='ignore'))
+])
+
+
+
+
+# date feature processing ---------------------------------------------------------
+
+date_feats = ['dayofweek', 'dayofyear',  'is_leap_year', 'quarter', 'weekofyear', 'year']
+
+class DateTransformer(BaseEstimator, TransformerMixin):
+    def fit(self, x, y=None):
+          return self
+
+    def transform(self, x):
+
+        x['GAME_DATE_EST'] = pd.to_datetime(x['GAME_DATE_EST'])
+
+        dayofweek = x.GAME_DATE_EST.dt.dayofweek
+        dayofyear= x.GAME_DATE_EST.dt.dayofyear
+        is_leap_year =  x.GAME_DATE_EST.dt.is_leap_year
+        quarter =  x.GAME_DATE_EST.dt.quarter
+        weekofyear = x.GAME_DATE_EST.dt.weekofyear
+        year = x.GAME_DATE_EST.dt.year
+
+        df_dt = pd.concat([dayofweek, dayofyear,  is_leap_year, quarter, weekofyear, year], axis=1)
+
+        return df_dt
+
+date_pipeline = Pipeline(steps=[
+    ('date', DateTransformer())
+])
+
+
+
+col_trans_pipeline = ColumnTransformer(
+    transformers=[
+        ('date', date_pipeline, ['GAME_DATE_EST']),
+        ('numeric', num_pipeline, rel_num_feats),
+        ('cat_low', cat_pipeline_low_card, rel_cat_feats_low_card)
+    ]
+)
+
+model = LinearRegression()
+
+pipeline = Pipeline(steps=[
+    ('preprocess', col_trans_pipeline),
+    ('model', model)
+])
+
+
+
+
+# Mlflow Tracking  ------------------------------------------------
+
+
+# mlflow server --host 127.0.0.1 --port 5000 --backend-store-uri sqlite:///mlflow.db   --artifacts-destination s3://nbadk-model/models/team-game/experiments
+# alternate source --default-artifact-root mlruns/ 
+local_server_uri = 'http://127.0.0.1:5000'
+mlflow.set_tracking_uri(local_server_uri)
+
 experiment_name = "team_game"
-#mlflow.create_experiment(experiment_name, artifact_location="s3://nbadk-model/models/team-game/experiments")
+
+mlflow.create_experiment(experiment_name, artifact_location="s3://nbadk-model/models/team-game/experiments")
 mlflow.set_experiment(experiment_name)
 
+with mlflow.start_run():
+
+    model_name_tag = "baseline_rolling_5_team_avg"
+
+    mlflow.log_param("model_name", model_name_tag)
+    #mlflow.log_param("features_used", ", ".join(X.columns))
+    mlflow.log_param("features_used", "none")
+    
+
+
+    cross_val_scores = cross_val_score(pipeline, X_train, y_train, cv=tscv)
+    cross_val_score_mean_sqaured_error = cross_val_score(pipeline, X_train, y_train, cv=tscv, scoring='neg_mean_squared_error')
+
+    #pipeline.fit(X_train, y_train)
+    #y_train_pred = pipeline.predict(X_train) 
+    #adjusted_r2 = 1 - ( 1-r2 ) * (len(y_train) - 1 ) / ( len(y_train) - X_train.shape[1] - 1 )
+    
+    mlflow.setcv_tag('mlflow.runName', model_name_tag) # set tag with run name so we can search for it later
+
+
+    mlflow.log_metric("mse", baseline_mse)
+    mlflow.log_metric("rmse", baseline_rmse)
+    mlflow.log_metric("mae", baseline_mae)
+    #mlflow.log_metric('adjusted_r2', adjusted_r2)
+    #mlflow.log_metric('cross_val_score_avg', cross_val_scores.mean())
+    #mlflow.log_metric('cross_val_score_rmse', np.mean(np.sqrt(np.abs(cross_val_score_mean_sqaured_error))))
+
+    mlflow.log_metric('rmse', rmse)
+    mlflow.log_metric('mae', mae)
+    mlflow.log_metric('r2', r2)
+
+    mlflow.sklearn.log_model(pipeline, model_name_tag)
 
 
 
@@ -198,65 +361,4 @@ mlflow.set_experiment(experiment_name)
 
 
 
-
-# what to name 
-A.set_index('key').join([B2, C2], how='inner').reset_index()
-
-
-
-boxscore_combined_trad = game_headers_df_processed_filtered.merge(boxscore_trad_team_df, left_on=['GAME_ID', 'HOME_TEAM_ID'], right_on=['GAME_ID', 'TEAM_ID'], how='left')
-
-boxscore_combined_adv = game_headers_df_processed_filtered.merge(boxscore_adv_team_df, left_on=['GAME_ID', 'HOME_TEAM_ID'], right_on=['GAME_ID', 'TEAM_ID'], how='left')
-
-boxscore_combined_trad[boxscore_combined_trad['PTS'].isnull()]['SEASON'].value_counts()
-
-missing_game_ids_trad = boxscore_combined_trad[boxscore_combined_trad['PTS'].isnull()]['GAME_ID'].unique()
-missing_game_df = game_headers_df_processed_filtered[game_headers_df_processed_filtered['GAME_ID'].isin(missing_game_ids_trad)]
-
-
-missing_game_ids_adv = boxscore_combined_adv[boxscore_combined_adv['PIE'].isnull()]['GAME_ID'].unique()
-
-len(missing_game_ids_trad)
-
-
-
-
-
-len(missing_game_ids_adv)
-
-
-boxscore_adv_error = get_boxscore_advanced(missing_game_ids_adv)
-boxscore_trad_error = get_boxscore_traditional(missing_game_ids_trad)
-
-boxscore_adv_error_remaining = get_boxscore_advanced(boxscore_adv_error)
-
-
-
-
-
-
-get_player_dfs()
-
-# need to check game IDS 
-
-
-
-# CHECK MISSING FUNCTIONS ---------------------------------------------
-
-
-trad_game_id_missing = [game_id for game_id in boxscore_trad_team_df.GAME_ID.unique() if game_id not in game_header_game_ids_complete]
-adv_game_id_missing = [game_id for game_id in boxscore_adv_team_df.GAME_ID.unique() if game_id not in game_header_game_ids_complete]
-
-
-adv_game_id_missing
-
-trad_missing_from_adv = [game_id for game_id in boxscore_trad_team_df.GAME_ID.unique() if game_id not in  boxscore_adv_team_df.GAME_ID.unique()]
-
-
-
-
-boxscore_adv_team_df = boxscore_adv_team_df.drop(['TEAM_NAME', 'TEAM_ABBREVIATION', 'TEAM_CITY', 'MIN'], axis=1)
-
-team_boxscore_combined = pd.merge(boxscore_trad_team_df, boxscore_adv_team_df, on=['GAME_ID', 'TEAM_ID'], how='outer')
-
-
+   
